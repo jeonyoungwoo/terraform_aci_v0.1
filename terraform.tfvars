49e2ad93e4e127823c452aa24fcb7be6f9436083
jeonyoungwoo <26229501+jeonyoungwoo@users.noreply.github.com>
2025-04-07 12:12:21 +0900

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+aci_username = "admin"
+aci_password = "Cisco123!@#"
+aci_url      = "https://10.1.200.109"
+
---
+++ varibles.tf
@@ -0,0 +1,14 @@
+variable "aci_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "aci_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "aci_url" {
+  type = string
+}
+
